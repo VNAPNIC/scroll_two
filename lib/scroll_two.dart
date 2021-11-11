6c88e84f1cdc43d6bb4f0422e861e0fa7b3aab1c
Nankai <nam.bh@financialdeepmind.com>
2021-11-11 13:33:28 +0700

    supoort web plugins

--- lib/scroll_two.dart
+++ lib/scroll_two.dart
@@ -3,5 +3,8 @@ library scroll_two;
 import 'package:flutter/material.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 import 'package:collection/collection.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 part 'src/scroll_two.dart';
